d686e054aae9a6259bbeebf7c07f0f253167b696
Paneer Selvam <99405385+paneer24@users.noreply.github.com>
2025-04-20 22:36:09 +0530

    Initial Commit

--- app/src/main/java/com/example/newsnow/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/newsnow/ui/navigation/AppNavigation.kt
@@ -12,11 +12,8 @@ fun AppNavigationGraph() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = Routes.HOME_SCREEN) {
         composable(Routes.HOME_SCREEN) {
-//inside this we will define the home screen but for that we will need home screen which we will generate4
-            //We will have a package inside ui named screen and make all the screens there
-            HomeScreen()
-            //if someone  trie to come to nav host and tries to navigate to Routes.HOME_SCREEN then they will be shown this home screen composable
-        }
 
+            HomeScreen()
+        }
     }
 }
