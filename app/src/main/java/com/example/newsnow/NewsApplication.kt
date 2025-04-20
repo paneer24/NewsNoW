3fe42e51d9d006067ef16c2417ccfb730b31cb03
Paneer Selvam <99405385+paneer24@users.noreply.github.com>
2025-04-20 22:26:48 +0530

    Initial Commit

--- app/src/main/java/com/example/newsnow/NewsApplication.kt
+++ app/src/main/java/com/example/newsnow/NewsApplication.kt
@@ -2,16 +2,18 @@ package com.example.newsnow
 
 import android.app.Application
 import android.util.Log
+import com.google.firebase.FirebaseApp
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class NewsApplication: Application() {
     override fun onCreate() {
         super.onCreate()
-        Log.d(TAG, "onCreate: ")
+        FirebaseApp.initializeApp(this)
+        Log.d(TAG, "Firebase initialized: ${FirebaseApp.getInstance() != null}")
     }
-    companion object{
-        private const val TAG="NewsApplication"
-
+    
+    companion object {
+        private const val TAG = "NewsApplication"
     }
 }
