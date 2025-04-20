12fc1b35711d8baedeef449cca4018597b0ef0a8
Paneer Selvam <99405385+paneer24@users.noreply.github.com>
2025-04-20 22:31:51 +0530

    Initial Commit

--- app/src/main/java/com/example/newsnow/data/entity/NewsResponse.kt
+++ app/src/main/java/com/example/newsnow/data/entity/NewsResponse.kt
@@ -1,24 +1,20 @@
 package com.example.newsnow.data.entity
-
 data class NewsResponse(
     val status:String,
     val totalResults:Int,
     val articles:List<Article>
-
 )
 data class Article(
-    val author: String?,        // Made nullable
-    val title: String,         // Usually not null
-    val description: String?,   // Made nullable
-    val url: String,           // Usually not null
-    val urlToImage: String?,    // Made nullable
-    val publishedAt: String,    // Usually not null
-    val content: String?,       // Made nullable
+    val author: String?,
+    val title: String,
+    val description: String?,
+    val url: String,
+    val urlToImage: String?,
+    val publishedAt: String,
+    val content: String?,
     val source: Source?
-
 )
 data class Source(
     val id:String?,
     val name:String
-
 )
